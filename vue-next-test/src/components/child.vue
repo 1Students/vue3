734850f916e4ea35c36b79b281c78af0edc19edf
858922308@qq.com <858922308@qq.com>
2021-04-24 14:09:15 +0800

    初次提交

---
+++ vue-next-test/src/components/child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      
+      <span>我是子组件</span>
+      <p><span>输入1</span>  <input type="text" v-model="name">  </p>
+      <p><span>输入2</span>  <input type="text" v-model="usid" >  </p>
+      <button @click="submit"> 登录  </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"child",
+  data(){
+    return{
+      name:'',
+      usid:'',
+    }
+  },
+  created(){
+
+  },
+  methods:{
+    submit(){
+      console.log(2223)
+      this.$emit("chi" ,this.name,this.usid )
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-next-test/src/components/father.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+       <div>
+      
+      <span>我是父组件组件</span>
+    
+     
+    <div>我发送给第一个组件的信息是:{{msgkk}}</div>
+      <ChildOne :adke="msgkk" /> 
+       <h2>{{ title }}</h2>
+  </div>
+  </div>
+</template>
+
+<script>
+// import ChildOne from "./HelloWorld"
+export default {
+    name:'father',
+    props: ['title'],
+ data(){
+    return{
+      name:'',
+      usid:'',
+      msgkk:"我是你ather",
+    }
+  },
+  created(){
+
+  },
+  methods:{
+    submit(){
+      console.log(2223)
+      this.$emit("chi" ,this.name,this.usid )
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
